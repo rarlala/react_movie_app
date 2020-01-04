e811b96cffd10b28c6b385d3fd9851859928d327
JEONGSUJI <wtw9611@gmail.com>
2020-01-05 03:21:51 +0900

    html/css 작업 및 slice 작업

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -3,18 +3,19 @@ import PropTypes from "prop-types";
 import "./Movie.css"
 
 function Movie({ year, title, summary, poster, genres}) {
-  return <div className="movie">
+  return (
+  <div className="movie">
     <img src={poster} alt={title} title={title} />
     <div className="movie__data">
       <h3 className="movie__title">{title}</h3>
       <h5 className="movie__year">{year}</h5>
-      <ul className="genres">
-        {genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}
+      <ul className="movie__genres">
+        {genres.map((genre, index) => 
+        <li key={index} className="genres__genre">{genre}</li>)}
       </ul>
-      <p className="movie__summary">{summary}</p>
+      <p className="movie__summary">{summary.slice(0, 180)}...</p>
     </div>
-  </div>
-  
+  </div>)
 }
 
 Movie.propTypes = {
